95872e797f4ebdf3440a45ee6ee3d732003fabf2
jhan-dev <jhan.technology@gmail.com>
2023-02-24 10:48:43 -0800

    Added styling, fonts, and checkboxes

--- app.js
+++ app.js
@@ -1,67 +1,81 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"]
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-    let today = new Date()
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
+  let day = today.toLocaleDateString("en-US", options);
 
-    let day = today.toLocaleDateString("en-US", options)
+  // let currentDay = today.getDay()
+  // let day = ""
 
-    // let currentDay = today.getDay()
-    // let day = ""
+  // switch (currentDay) {
+  //     case 0:
+  //         day = "Sunday"
+  //         break;
+  //     case 1:
+  //         day = "Monday"
+  //         break;
+  //     case 2:
+  //         day = "Tuesday"
+  //         break;
+  //     case 3:
+  //         day = "Wednesday"
+  //         break;
+  //     case 4:
+  //         day = "Thursday"
+  //         break;
+  //     case 5:
+  //         day = "Friday"
+  //         break;
+  //     case 6:
+  //         day = "Saturday"
+  //         break;
+  //         default:
+  //             console.log(`Error: current day is equal to: ${currentDay}`)
+  // }
 
-    // switch (currentDay) {
-    //     case 0:
-    //         day = "Sunday"
-    //         break;
-    //     case 1:
-    //         day = "Monday"
-    //         break;
-    //     case 2:
-    //         day = "Tuesday"
-    //         break;
-    //     case 3:
-    //         day = "Wednesday"
-    //         break;
-    //     case 4:
-    //         day = "Thursday"
-    //         break;
-    //     case 5:
-    //         day = "Friday"
-    //         break;
-    //     case 6:
-    //         day = "Saturday"
-    //         break;
-    //         default:
-    //             console.log(`Error: current day is equal to: ${currentDay}`)
-    // }
+  res.render("list", { listTitle: day, newListItems: items });
+});
 
-    res.render("list", {kindOfDay: day, newListItems: items})
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
 
-})
+  if (req.body.list === "Work") {
+    workItems.push(item);
+  }
+  else {
+    items.push(item);
+    res.redirect("/");
+  }
+  console.log(item);
+});
 
-app.post("/", function(req, res){
-    let item = req.body.newItem
-    console.log(item)
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
 
-    items.push(item)
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/");
+});
 
-    res.redirect("/")
-})
-
-app.listen(3000, function(){
-    console.log("server is running on port 3000.")
-})
+app.listen(3000, function () {
+  console.log("server is running on port 3000.");
+});
