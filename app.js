4595a4639408a76d8b03fed4ad1014b7f42bc14a
jhan-dev <jhan.technology@gmail.com>
2023-03-16 16:12:37 -0700

    lodash added to handle upper+lowercase inputs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
@@ -95,7 +96,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName})
     .then(function(foundList){
